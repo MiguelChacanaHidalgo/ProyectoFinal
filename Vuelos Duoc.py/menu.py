343aa95acad4525036476ecd3e02c0d04ad5e1d0
MiguelChacanaHidalgo <mig.chacana@duocuc.cl>
2023-06-18 16:38:55 -0400

    si

---
+++ Vuelos Duoc.py/menu.py
@@ -0,0 +1,11 @@
+while True:
+  try:
+   print("Menú Principal")
+   print("1. Ver asientos disponibles")
+   print("2. Comprar asiento")
+   print("3. Anular vuelo")
+   print("4. Modificar datos de pasajero")
+   print("5. Salir")
+   opcion = int(input("Ingrese una opción: "))
+  except:
+   print("DEBE INGRESAR UNA DE LAS OPCIONES")
