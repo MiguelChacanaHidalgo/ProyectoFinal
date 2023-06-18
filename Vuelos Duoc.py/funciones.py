07b6e8c22f239afee1ff59b22f1601dead33d174
MiguelChacanaHidalgo <134889081+MiguelChacanaHidalgo@users.noreply.github.com>
2023-06-18 17:03:19 -0400

    Create funciones.py

---
+++ Vuelos Duoc.py/funciones.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def asientos():
+ asientos = np.zeros((5,6))
+ asientosVip = np.zeros((2,6))
+   
+
+ print(asientos)
+ print("  -------VIP------")
+ print(asientosVip)
+ return
