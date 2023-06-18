fc63e1db588c70b591245a158c8bff07edf082ac
MiguelChacanaHidalgo <134889081+MiguelChacanaHidalgo@users.noreply.github.com>
2023-06-18 19:11:50 -0400

    Update menu.py

--- Vuelos Duoc.py/menu.py
+++ Vuelos Duoc.py/menu.py
@@ -1,3 +1,5 @@
+import funciones as fn
+
 while True:
   try:
    print("Menú Principal")
@@ -9,3 +11,20 @@ while True:
    opcion = int(input("Ingrese una opción: "))
   except:
    print("DEBE INGRESAR UNA DE LAS OPCIONES")
+
+  try:
+    if opcion == 1:
+          fn.mostrar_asientos()
+    elif opcion == 2:
+          fn.comprar_asiento()
+    elif opcion == 3:
+          fn.anular_vuelo()
+    elif opcion == 4:
+          fn.modificar_datos()
+    elif opcion == 5:
+          print("¡Gracias por utilizar nuestro sistema de venta de pasajes vuelva pronto!")
+          break
+    else:
+          print("Opción inválida.")
+  except:
+    print()
